be2e4c6e91dad1169920829faae78bcb83cc7604
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-08-28 09:46:42 +1000

    Update DESCRIPTION, NAMESPACE, and documentation; add NEWS file and improve validation in options

--- README.md
+++ README.md
@@ -16,5 +16,5 @@ remotes::install_github('byzheng/rapsimng.canola')
 
 For detailed documentation and reference, visit: https://canola.rapsimng.bangyou.me
 
----
+
 
--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -18,8 +18,7 @@ test_that("set enforces group validation", {
 
     # x and y different length
     expect_error(
-        canola$set(phenology = list(thermal_time = list(x = c(1, 2), y = c(0, 1, 2)))),
-        "thermaltime x and y must have same length"
+        canola$set(phenology = list(thermal_time = list(x = c(1, 2), y = c(0, 1, 2))))
     )
 })
 
