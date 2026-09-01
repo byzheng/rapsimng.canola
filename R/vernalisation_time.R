76b2800c1a8e5fba451020fad0709f7c0e973548
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-09-01 14:55:12 +1000

    Fix documentation link for APSIM Next Generation Canola

--- R/vernalisation_time.R
+++ R/vernalisation_time.R
@@ -5,7 +5,7 @@
 #' The daily vernalisation time is calculated by interpolating the effective vernalisation time at three hour intervals between the daily minimum and maximum temperatures, and then averaging these values to get the daily vernalisation time. 
 #' 
 #' For more information about the canola phenology model, see the 
-#' [APSIM Next Generation Canola Documentation](https://apsimng-notes.bangyou.me/docs/Plants/Canola/Phenology/Vegetative.html).
+#' [APSIM Next Generation Canola Documentation](https://notes.apsimng.bangyou.me/docs/Plants/Canola/Phenology/Vegetative.html).
 #' 
 #' 
 #' @param mint The minimum temperature
